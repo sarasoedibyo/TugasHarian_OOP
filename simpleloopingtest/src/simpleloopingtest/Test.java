5855619a6f39484de44a11df6a308d2c7de8aba4
sarasoedibyo <101959202+sarasoedibyo@users.noreply.github.com>
2022-04-08 23:57:32 +0800

    Add files via upload

---
+++ simpleloopingtest/src/simpleloopingtest/Test.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package simpleloopingtest;
+
+/**
+ *
+ * @author Gabriella Sara
+ */
+public class Test {
+
+   public static void main(String args[]) {
+      int x = 17;
+
+      if( x > 18 ) {
+         System.out.print("You are allowed to join this game");
+      }else if( x < 18 ) {
+         System.out.print("You are not allowed to join this game");
+      }
+   }
+}
